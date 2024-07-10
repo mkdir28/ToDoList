cf14902fe5cc57506354b77a76690ecbc582fc9e
Maryna Kamienieva <mkdirwd@gmail.com>
2024-07-10 13:19:12 +0200

    added Extention for colors and Option.swift with varaibles

---
+++ ToDoList/Extentions/Date+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Ext.swift
+//  ToDoList
+//
+//  Created by Marina on 10.07.2024.
+//
+
+import Foundation
---
+++ ToDoList/Models/Option.swift
@@ -0,0 +1,8 @@
+//
+//  Option.swift
+//  ToDoList
+//
+//  Created by Marina on 10.07.2024.
+//
+
+import Foundation
