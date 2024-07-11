d55052c208b54b2db92d645c680d99e6a5736862
Maryna Kamienieva <mkdirwd@gmail.com>
2024-07-11 18:07:38 +0200

    updated Model

--- ToDoList/Models/Option.swift
+++ ToDoList/Models/Option.swift
@@ -5,4 +5,13 @@
 //  Created by Marina on 10.07.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct Option: Codable, Identifiable{
+    var id = UUID()
+    var taskName: String
+    var isCompleted = false
+    var dedline: Date?
+    var byCompletionDate: Date?
+    var byCreationDate = Date()
+}
