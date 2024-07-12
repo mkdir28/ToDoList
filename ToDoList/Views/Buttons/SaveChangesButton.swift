3aa36823a62adb5789cc002525ef2f36a791cb55
Maryna Kamienieva <mkdirwd@gmail.com>
2024-07-12 12:49:00 +0200

    added new custom button for savings

--- ToDoList/Views/Buttons/SaveChangesButton.swift
+++ ToDoList/Views/Buttons/SaveChangesButton.swift
@@ -5,4 +5,22 @@
 //  Created by Marina on 12.07.2024.
 //
 
-import Foundation
+import SwiftUI
+struct SaveChangesButton: View {
+    
+    var body: some View {
+        Text(Info.EditButton.text)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 260, height: 50)
+            .foregroundColor(.white)
+            .background(Color.orange)
+            .cornerRadius(10)
+    }
+}
+
+struct SaveChangesButton_Previews: PreviewProvider{
+    static var previews: some View {
+        SaveChangesButton()
+    }
+}
