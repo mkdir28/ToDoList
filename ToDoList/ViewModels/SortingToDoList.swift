9308b7227094c638ef24e5cb7c0eb26c923df6df
Maryna Kamienieva <mkdirwd@gmail.com>
2024-07-13 21:21:13 +0200

    updated enum of sorting

--- ToDoList/ViewModels/SortingToDoList.swift
+++ ToDoList/ViewModels/SortingToDoList.swift
@@ -8,82 +8,9 @@
 import SwiftUI
 import SwiftData
 
-enum SortingToDoList{
-    case byCompletion
-    case byDeadline
-    case byCreation
-}
-
-struct SortingPreview: View {
-    
-    @StateObject var viewModel = ToDoListViewModel()
-    
-    @State private var sortSelection: SortingToDoList?
-    
-    @State private var sorting = false
-    
-    private func presentSortView() {
-        sorting.toggle()
-    }
-    
-    var body: some View {
-        Button(action: presentSortView) {
-            SortButton()
-                .padding(.init(top: -50,
-                               leading: 130,
-                               bottom: 0,
-                               trailing: -300)
-                )
-        }
-        .sheet(isPresented: $sorting) {
-            VStack {
-                Button(action: {
-                    sortSelection = .byCompletion
-                    viewModel.sorting(sort: .byCompletion)
-                    sorting.toggle()
-                }) {
-                    Text(Info.Sorting.byCompletion)
-                        .foregroundColor(Color.infoColor)
-                        .multilineTextAlignment(.leading)
-                        .font(.setFont(size: 20,
-                                       weight: .bold))
-                }
-                .padding()
-                
-                Button(action: {
-                    sortSelection = .byDeadline
-                    viewModel.sorting(sort: .byDeadline)
-                    sorting.toggle()
-                }) {
-                    Text(Info.Sorting.byDeadline)
-                        .foregroundColor(Color.infoColor)
-                        .multilineTextAlignment(.leading)
-                        .font(.setFont(size: 20,
-                                       weight: .bold))
-                }
-                .padding()
-                
-                Button(action: {
-                    sortSelection = .byCreation
-                    viewModel.sorting(sort: .byCreation)
-                    sorting.toggle()
-                }) {
-                    Text(Info.Sorting.byCreation)
-                        .foregroundColor(Color.infoColor)
-                        .multilineTextAlignment(.leading)
-                        .font(.setFont(size: 20,
-                                       weight: .bold))
-                }
-                .padding()
-            }
-        }
-        
-        
-    }
-}
-
-struct SortingPreview_Previews: PreviewProvider {
-  static var previews: some View {
-      SortingPreview()
-  }
+enum SortingToDoList: String, CaseIterable{
+    case byCompletion = "Completion"
+    case byDeadline = "Deadline"
+    case byCreation = "Creation"
+    case all = "All"
 }
