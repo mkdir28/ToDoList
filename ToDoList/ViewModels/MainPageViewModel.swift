a7b00235ce8341efa6e40aea399df9165c91f22d
Maryna Kamienieva <mkdirwd@gmail.com>
2024-07-14 02:32:21 +0200

    updated contnt view and added new custom button

---
+++ ToDoList/ViewModels/MainPageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainPageViewModel.swift
+//  ToDoList
+//
+//  Created by Marina on 14.07.2024.
+//
+
+import Foundation
---
+++ ToDoList/Views/Buttons/GetStartedButton.swift
@@ -0,0 +1,8 @@
+//
+//  GetStartedButton.swift
+//  ToDoList
+//
+//  Created by Marina on 14.07.2024.
+//
+
+import Foundation
